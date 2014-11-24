3b108d1cb51ec026b7b1c0f615fac3d6fd92c136
Tian Xie <tian@animoto.com>
2014-11-24 14:27:59 -0500

    Simplified controller

--- word-machine-controller.js
+++ word-machine-controller.js
@@ -6,29 +6,25 @@ var path = require('path');
 var Async = require('async');
 var WordMachine = require(path.resolve(__dirname, "word-machine"));
 
-var wordMachineFunction = function(logger) {
-  var wm = new WordMachine(logger);
-  return function(callback) {
-    wm.initialize(path.resolve(__dirname, "texts"), function(error) {
-      callback(error, wm);
-    });
-  };
-};
-
 var showFunction = function(logger) {
   return function(req, res) {
     Async.waterfall(
       [
-        wordMachineFunction(logger),
+        function(callback) {
+          var wm = new WordMachine(logger);
+          wm.initialize(path.resolve(__dirname, "texts"), function(error) {
+            callback(error, wm);
+          });
+        },
         function(wm, callback) {
           wm.randPayload(callback);
         }
       ],
       function(error, payload) {
         if (error) {
-          res.status(400).send({"error" : {"message" : error.message }});
+          res.status(400).json({"error" : {"message" : error.message }});
         } else {
-          res.status(200).send({"payload" : payload});
+          res.status(200).json({"payload" : payload});
         }
       }
     );
@@ -37,37 +33,25 @@ var showFunction = function(logger) {
 
 var verifyFunction = function(logger) {
   return function(req, res) {
-    Async.waterfall(
-      [
-        wordMachineFunction(logger),
-        function(wm, callback) {
-          var payload = req.param("payload");
-          logger.debug(payload);
-          if (typeof(payload) == "undefined") {
-            callback(new Error("Missing payload"));
-          } else if (!wm.verifyPayload(payload)) {
-            callback(new Error("Invalid token - stop trying to cheat!"));
-          } else {
-            var expected = WordMachine.wordCount(payload.text, payload.words);
-            var actual = payload.word_count;
+    var wm = new WordMachine(logger);
+    var payload = req.param("payload");
+    logger.debug("Received payload:", payload);
 
-            if (!_.isEqual(actual, expected)) {
-              logger.debug(JSON.stringify({"expected" : expected, "actual" : actual}));
-              callback(new Error("Invalid count"));
-            } else {
-              callback();
-            }
-          }
-        }
-      ],
-      function(error) {
-        if (error) {
-          res.status(400).send({"error" : {"message" : error.message }});
-        } else {
-          res.sendStatus(200);
-        }
+    if (typeof(payload) == "undefined") {
+      res.status(400).json({"error" : {"message" : "Missing payload" }});
+    } else if (!wm.verifyPayload(payload)) {
+      res.status(400).json({"error" : {"message" : "Invalid token - stop trying to cheat!" }});
+    } else {
+      var expected = WordMachine.wordCount(payload.text, payload.words);
+      var actual = payload.word_count;
+
+      if (!_.isEqual(actual, expected)) {
+        logger.debug(JSON.stringify({"expected" : expected, "actual" : actual}));
+        res.status(400).json({"error" : {"message" : "Invalid count" }});
+      } else {
+        res.sendStatus(200);
       }
-    );
+    }
   };
 };
 
